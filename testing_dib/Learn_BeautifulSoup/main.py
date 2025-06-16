e34237bfde8c0e980b2daebb693da562e5a15af7
dibakash <dibakash@gmail.com>
2025-06-16 18:22:39 +0530

    learning beautiful soup in Learn_BeautifulSoup Folder

--- testing_dib/Learn_BeautifulSoup/main.py
+++ testing_dib/Learn_BeautifulSoup/main.py
@@ -1,5 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 def main():
     print("Hello from learn-beautifulsoup!")
+    url = "https://www.scrapethissite.com/pages/forms/"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        print(soup.prettify())
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
 
 if __name__ == "__main__":
