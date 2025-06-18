18055403fa42fe0f90a9934c0c6d2a0a284c1e09
dibakash <dibakash@gmail.com>
2025-06-18 20:53:15 +0530

    updated folders

--- testing_abm/perplex.py
+++ testing_abm/perplex.py
@@ -12,10 +12,7 @@ import json
 with open("cookiesTopic.txt", "r") as file:
     cookie = file.read().strip()
 
-headers = {
-    "Cookie": cookie,
-    "Accept": "application/json"
-}
+headers = {"Cookie": cookie, "Accept": "application/json"}
 
 base_url = "https://discourse.onlinedegree.iitm.ac.in/search.json?q=%23courses%3Atds-kb%20after%3A2025-01-01%20order%3Aviews"
 all_topics = []
@@ -30,14 +27,11 @@ while True:
         print(response.text[:500])  # Print first 500 chars for debugging
         break
     data = response.json()
-    if 'topics' not in data or not data['topics']:
+    if "topics" not in data or not data["topics"]:
         break
-    for topic in data['topics']:
-        all_topics.append({
-            "topic_id": topic['id'],
-            "created_at": topic['created_at']
-        })
+    for topic in data["topics"]:
+        all_topics.append({"topic_id": topic["id"], "created_at": topic["created_at"]})
     page += 1
 
-with open("discourseTopics.json", "w") as file:
+with open("discourseTopics2.json", "w") as file:
     json.dump(all_topics, file, indent=4)
